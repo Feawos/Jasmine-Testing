806b7c4a55ac8ac7cd288786dddb45f1fc54f0b2
OluwafemiAwosanya <oluwafemiawosanya@hotmail.com>
2021-01-01 22:24:45 +0000

    deleted drink about files from project

--- scripts/drinkAbout.js
+++
@@ -1,7 +0,0 @@
-whatCanIDrink = function(age) {
-    if(typeof(age) == "number") {
-        return age
-    } else {
-        alert("Sorry. I can’t tell what drink because that age is incorrect!");
-    }
-}
--- spec/drinkAboutSpec.js
+++
@@ -1,8 +0,0 @@
-describe ("whatCanIDrink", function(){
-
-})
-    describe("Age tests", function(){
-        it("should return 'Sorry. I can’t tell what drink because that age is incorrect!' ", function(){
-            expect(age).toBe(0);
-        })
-    })
